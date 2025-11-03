368e11fb004cd5d1079024ce71acbea6484bd671
Peerapat Krairat <peerapat.krairat@gmail.com>
2025-11-03 21:43:05 +0700

    fixed: remove unused components

--- src/hooks/use-confirm.tsx
+++ src/hooks/use-confirm.tsx
@@ -7,7 +7,6 @@ import {
     DialogDescription,
     DialogFooter,
     DialogHeader,
-    DialogOverlay,
     DialogTitle,
 } from "@/components/ui/dialog";
 import { useTranslation } from "react-i18next";
